515fc1eb5b2fdf4390a6fbf42b5567d6f80ef5e4
LAPTOP-LMLMH24D\elmar <elmardusaev>
2023-12-04 11:37:00 +0300

    Add Quadrilateral

---
+++ ShapeLib/ShapeHelpers/QuadrilateralHelper.cs
@@ -0,0 +1,13 @@
+﻿using ShapeLib.ShapeIdentifier;
+using ShapeLib.Shapes;
+
+namespace ShapeLib.ShapeHelpers
+{
+    internal class QuadrilateralHelper : BaseHelper<Quadrilateral>
+    {
+        public QuadrilateralHelper(Quadrilateral shape, ICanIdentifyType<Quadrilateral> typeIdentifier) : base(shape, typeIdentifier)
+        {
+
+        }
+    }
+}
---
+++ ShapeLib/ShapeIdentifier/QuadrilateralTypeIdentifier.cs
@@ -0,0 +1,12 @@
+﻿using ShapeLib.Shapes;
+
+namespace ShapeLib.ShapeIdentifier
+{
+    internal class QuadrilateralTypeIdentifier : ICanIdentifyType<Quadrilateral>
+    {
+        public string Identify(Quadrilateral figure)
+        {
+            return string.Empty;
+        }
+    }
+}
